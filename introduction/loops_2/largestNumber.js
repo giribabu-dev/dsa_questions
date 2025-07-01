048a20310227e138ae1af281975689dcfb0dd1ab
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-01 22:46:13 +0530

    loops-2

---
+++ introduction/loops_2/largestNumber.js
@@ -0,0 +1,12 @@
+/* Find Largest Number in an Array */
+
+
+const arr = [10, 44, 7, 1, 20, 25];
+
+let largestNum;
+
+function findLargestNumber(arr){
+
+};
+
+findLargestNumber(arr);
---
+++ introduction/loops_2/smallestNumber.js
@@ -0,0 +1,12 @@
+/* Find Smallest Number in an Array */
+
+
+const arr = [10, 42, 7, 1, 20, 25];
+
+let smallestNum;
+
+function findSmallestNumber(arr){
+
+};
+
+findSmallestNumber(arr);
