f8e26ee0d81f3b9660166232f479e0a56572bea0
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-02 10:52:14 +0530

    loops2

--- introduction/loops_2/largestNumber.js
+++ introduction/loops_2/largestNumber.js
@@ -1,12 +1,27 @@
-/* Find Largest Number in an Array */
+/* Q3. Find Largest Number in an Array */
+
+/* 
+    1. Initialize the variable with the first element of the array as the largest
+    2. Loop through the array
+    3. Compare the each element with the current element
+    4. If an element is larger, update the large
+*/
 
 
 const arr = [10, 44, 7, 1, 20, 25];
 
-let largestNum;
+let large = arr[0];
 
 function findLargestNumber(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(large < arr[i]){
+            large = arr[i];
+        }
+    }
 
+    return large;
 };
 
-findLargestNumber(arr);
+let largestNum = findLargestNumber(arr);
+
+console.log(largestNum);
--- introduction/loops_2/smallestNumber.js
+++ introduction/loops_2/smallestNumber.js
@@ -1,12 +1,27 @@
-/* Find Smallest Number in an Array */
+/* Q2. Find Smallest Number in an Array */
+
+/*
+    1. Initialize a variable with the first element of the array as the minimum
+    2. Loop through the array
+    3. Compare each element with the currect element
+    4. If an element is smaller, update the minimum
+*/
 
 
-const arr = [10, 42, 7, 1, 20, 25];
+const arr = [10, 43, 7, 2, 20, 25];
 
-let smallestNum;
+let small = arr[0];
 
 function findSmallestNumber(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(small > arr[i]){
+            small = arr[i]
+        }
+    }
 
+    return small;
 };
 
-findSmallestNumber(arr);
+let smallestNum = findSmallestNumber(arr);
+
+console.log(smallestNum)
