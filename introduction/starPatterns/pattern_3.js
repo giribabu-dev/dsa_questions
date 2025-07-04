e339812e004d28ceeac19b0059c076fabb9a9306
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-04 23:14:22 +0530

    star pattern 3

--- introduction/starPatterns/pattern_3.js
+++ introduction/starPatterns/pattern_3.js
@@ -1,16 +1,17 @@
 /*
-    *
-    * *
-    * * *
-    * * * *
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
 */
 
-let n = 4;
+let n = 5;
 
 for (let i = 0; i < n; i++) {
     let row = "";
     for (let j = 0; j <= i; j++) {
-        row = row + "*";
+        row = row + (j + 1);
     }
 
     console.log(row);
