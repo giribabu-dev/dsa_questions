0aa02ec3e529c25708ed01b14e0d5f82483b1dd6
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-09 15:15:16 +0530

    count digits

---
+++ introduction/count_digit/countDigits.js
@@ -0,0 +1,21 @@
+/* Count the number of digits in an integer */
+
+let input = 31007092025;
+
+function countDigits(num) {
+
+    if (num == 0) return 1;
+
+    num = Math.abs(num);
+    let count = 0;
+
+    while (num > 0) {
+        num = Math.floor(num / 10);
+        count++;
+    };
+
+    return count;
+};
+
+const result = countDigits(input);
+console.log(result);
