5aed7967e0d8426c02bdfad79f6ab5c28f6bc1a8
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-15 10:49:54 +0530

    modifications

--- introduction/star_patterns/pattern_7.js
+++ introduction/star_patterns/pattern_7.js
@@ -8,16 +8,19 @@
 
 let n = 5;
 
-for (let i = 0; i < n; i++){
-    let row = "";
+for (let i = 0; i < n; i++) {
 
-    for (let j = 0; j < n - (i + 1); j++){
-        row = row + " ";
-    };
+  let row = "";
 
-    for (let k = 0; k < (i + 1); k++){
-        row = row + "*";
-    };
+  for (let j = 0; j < n - (i + 1); j++) {
+    row = row + " ";
+  };
 
-    console.log(row);
-}
+  for (let k = 0; k < (i + 1); k++) {
+    row = row + "*";
+  };
+
+  console.log(row);
+  
+};
+
