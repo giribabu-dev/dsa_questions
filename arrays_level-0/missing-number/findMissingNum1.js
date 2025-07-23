5843e9a7aaa642ace65905151370a4eddbb79446
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-23 12:27:46 +0530

    Added Missing Number in arrays_level-0

---
+++ arrays_level-0/missing-number/findMissingNum1.js
@@ -0,0 +1,19 @@
+/* 
+    Missing Number
+    Q: Find the missing number in a sequence from 0 to N.
+*/
+
+function findMissingNum(arr) {
+
+    const n = arr.length;
+    const expectedSum = (n * (n + 1)) / 2;
+
+    const actualSum = arr.reduce((acc, num) => acc + num, 0);
+
+    return expectedSum - actualSum;
+};
+
+console.log(findMissingNum([3, 0, 1]));
+console.log(findMissingNum([0, 1]));
+console.log(findMissingNum([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+console.log(findMissingNum([0]));
