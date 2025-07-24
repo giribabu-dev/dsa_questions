52522c3d30a763acdcf7439b39215f3a789bd359
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-24 12:49:13 +0530

    Added 8.single-number in Arrays_easy-medium

---
+++ arrays_easy-medium/8. single-number/uniqueElements.js
@@ -0,0 +1,18 @@
+/*
+    Single Number
+    Que: Find the unique element in an array where all others are repeated.
+*/
+
+function findUniqueElement(arr){
+
+    let result = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        result = result ^ arr[i];
+    }
+
+    return result;
+};
+
+console.log(findUniqueElement([2, 3, 5, 4, 5, 3, 4]));
+console.log(findUniqueElement([2, 3, 2, 4, 4]));
