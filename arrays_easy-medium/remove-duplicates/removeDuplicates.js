145d3eb32b4b7f5cf8e8fb712d437026b4f2b2fb
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-24 11:58:05 +0530

    Added 1 file in remove-duplicates

---
+++ arrays_easy-medium/remove-duplicates/removeDuplicates.js
@@ -0,0 +1,25 @@
+/* 
+    Remove Duplicates
+    Q: Remove duplicate elements from a sorted array
+*/
+
+function removeDuplicates(arr) {
+
+    // Sort the array in numerical order
+    arr.sort((a, b) => a - b);
+
+    let output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        // Only add element if it is not equal to previous element
+        if (i === 0 || arr[i] !== arr[i - 1]) {
+            output.push(arr[i]);
+        }
+    };
+
+    return output;
+};
+
+console.log(removeDuplicates([1, 2, 3, 2, 4, 1, 5]));
+console.log(removeDuplicates([1, 1, 5, 0, 7, 2, 4, 2, 0, 2, 5]));
---
+++ arrays_easy-medium/remove-duplicates/removeDuplicates1.js
@@ -0,0 +1,22 @@
+/*
+    Remove Duplicates
+    Que: Remove duplicate elements from a sorted array
+*/
+
+function removeDuplicates(arr){
+    
+    arr.sort((a, b) => a - b);
+
+    let output = [];
+
+    arr.forEach(ele => {
+        if(!output.includes(ele)){
+            output.push(ele);
+        }
+    });
+
+    return output;
+};
+
+console.log(removeDuplicates([1, 2, 3, 2, 4, 1, 5]));
+console.log(removeDuplicates([1, 1, 5, 0, 7, 2, 4, 2, 0, 2, 5]));
