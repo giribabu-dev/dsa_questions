01d2aca041ca507b7b4dedaafa067690a5b75c29
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-28 14:55:37 +0530

    Changes in Move Zeros To End

--- arrays_easy-medium/5. move-zeros/moveZerosEnd.js
+++ arrays_easy-medium/5. move-zeros/moveZerosEnd.js
@@ -3,19 +3,19 @@
     Question: Move all zeros to the end of the array without changing order.
 */
 
-function moveZerosToEnd(arr){
+function moveZerosToEnd(arr) {
     let nonZeroIndex = 0;
 
     // Move all non-zero element to front
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== 0){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
             arr[nonZeroIndex] = arr[i];
             nonZeroIndex++;
         }
     };
 
     // Fiil the remaining positions with zeros
-    while(nonZeroIndex < arr.length){
+    while (nonZeroIndex < arr.length) {
         arr[nonZeroIndex] = 0;
         nonZeroIndex++
     };
