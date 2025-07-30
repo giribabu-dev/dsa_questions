5135545a72dabbe25f594df6d62e39eb1bf6c389
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-30 14:54:04 +0530

    Changes in Missing-Number

--- arrays_easy-medium/7. missing-number/findMissingNum.js
+++ arrays_easy-medium/7. missing-number/findMissingNum.js
@@ -5,16 +5,16 @@
 
 function findMissingNum(arr) {
     
-    const totalElements = arr.length + 1;
-    const expectedSum = (totalElements * (totalElements + 1)) / 2;
+    const totalElements = arr.length + 1
+    const expectedSum = (totalElements * (totalElements + 1)) / 2
 
-    let actualSum = 0;
-    arr.forEach(ele => actualSum += ele);
+    let actualSum = 0
+    arr.forEach(ele => actualSum += ele)
 
-    return expectedSum - actualSum;
-};
+    return expectedSum - actualSum
+}
 
-console.log(findMissingNum([1, 2, 4, 5, 6]));
-console.log(findMissingNum([7, 2, 1, 4, 6, 3]));
-console.log(findMissingNum([1, 2, 3, 4, 5, 7, 8, 9, 10]));
-console.log(findMissingNum([1, 2, 3, 4, 5, 6, 7, 9, 10, 11]));
+console.log(findMissingNum([1, 2, 4, 5, 6]))
+console.log(findMissingNum([7, 2, 1, 4, 6, 3]))
+console.log(findMissingNum([1, 2, 3, 4, 5, 7, 8, 9, 10]))
+console.log(findMissingNum([1, 2, 3, 4, 5, 6, 7, 9, 10, 11]))
--- arrays_easy-medium/7. missing-number/findMissingNum1.js
+++ arrays_easy-medium/7. missing-number/findMissingNum1.js
@@ -5,15 +5,15 @@
 
 function findMissingNum(arr) {
 
-    const n = arr.length;
-    const expectedSum = (n * (n + 1)) / 2;
+    const n = arr.length
+    const expectedSum = (n * (n + 1)) / 2
 
-    const actualSum = arr.reduce((acc, num) => acc + num, 0);
+    const actualSum = arr.reduce((acc, num) => acc + num, 0)
 
-    return expectedSum - actualSum;
-};
+    return expectedSum - actualSum
+}
 
-console.log(findMissingNum([3, 0, 1]));
-console.log(findMissingNum([0, 1]));
-console.log(findMissingNum([9, 6, 4, 2, 3, 5, 7, 0, 1]));
-console.log(findMissingNum([0]));
+console.log(findMissingNum([3, 0, 1]))
+console.log(findMissingNum([0, 1]))
+console.log(findMissingNum([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+console.log(findMissingNum([0]))
