74632a4073c3768fbbedaea3ee10cc3727b09e99
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-31 14:26:39 +0530

    Changes in Single-Number

--- arrays-Easy_Medium/8. single-number/uniqueElements.js
+++ arrays-Easy_Medium/8. single-number/uniqueElements.js
@@ -3,16 +3,14 @@
     Que: Find the unique element in an array where all others are repeated.
 */
 
-function findUniqueElement(arr){
+function findUniqueElement(arr) {
+    let result = 0
 
-    let result = 0;
-
-    for(let i = 0; i < arr.length; i++){
-        result = result ^ arr[i];
+    for (let i = 0; i < arr.length; i++) {
+        result = result ^ arr[i]
     }
-
-    return result;
+    return result
 };
 
-console.log(findUniqueElement([2, 3, 5, 4, 5, 3, 4]));
-console.log(findUniqueElement([2, 3, 2, 4, 4]));
+console.log(findUniqueElement([2, 3, 5, 4, 5, 3, 4]))
+console.log(findUniqueElement([2, 3, 2, 4, 4]))
