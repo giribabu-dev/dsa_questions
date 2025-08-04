e7d720ad67b81ae4dcfbd51631fdfba80a326a5d
giribabu-a <giribabuannapareddi@gmail.com>
2025-08-04 12:37:14 +0530

    sum of first n numbers

--- recursion-Easy_Medium/2. sum-of-first-n-numbers/sumOfNums.js
+++ recursion-Easy_Medium/2. sum-of-first-n-numbers/sumOfNums.js
@@ -1,14 +1,17 @@
 /*
-    Sum of first n numbers
-    Que: Calculate the sum of the first n natural numbers using recursion.
+    Function to calculate sum of first n natural numbers using recursion
+    Que: Write a function sum(n) that calculates the sum of the first n natural numbers using recursion.
 
-    sum(n) = n + sum(n-1)
+    Concepts::
+    1) Recursion: A technique where a function calls itself with a reduced subproblem.
+    2) Base Case: Stops recursion to prevent infinite calls. Here, if n === 0, return 0.
+    3) Recursive Case: Return n + sum(n - 1)
 */
 
 function sum(n) {
-    if (n === 0) return 0
-    return n + sum(n - 1)
+    if (n === 0) return 0;
+    return n + sum(n - 1);
 }
 
-console.log(sum(5))
-console.log(sum(11))
+console.log(sum(5)); // 15
+console.log(sum(11)); // 66
