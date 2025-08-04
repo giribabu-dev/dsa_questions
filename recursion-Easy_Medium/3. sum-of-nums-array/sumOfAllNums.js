154966c566fa0b7548f9f3262fc71a1084512cb9
giribabu-a <giribabuannapareddi@gmail.com>
2025-08-04 13:19:45 +0530

    sum of numbers in array

--- recursion-Easy_Medium/3. sum-of-nums-array/sumOfAllNums.js
+++ recursion-Easy_Medium/3. sum-of-nums-array/sumOfAllNums.js
@@ -1,16 +1,20 @@
 /*
-    Sum of All Numbers in Array
-    Que: Sum of all numbers in array using Recursion
+    Sum of all numbers in array using Recursion
+    Que: Write a function sum(n) that calculates the sum of all numbers in an array arr
+        using recursion. It sums from index 0 to n. 
 
-    sum(n) = arr[n] + sum(n-1)
+    Concepts::
+    1) Recursion: The function keeps summing the element at index n and calls itself with n-1.
+    2) Base Case: If n==0, return the first element.
+    3) Recursive case: Return arr[n] + sum(n - 1)
 */
 
-const arr = [5, 3, 2, 0, 1]
+let arr = [5, 3, 2, 0, 1];
 
-function sum(n){
-    if(n == 0) return arr[n]
+function sum(n) {
+    if (n == 0) return arr[n];
 
-    return arr[n] + sum(n - 1)
+    return arr[n] + sum(n - 1);
 }
 
-console.log(sum(arr.length - 1))
+console.log(sum(arr.length - 1)) //11
