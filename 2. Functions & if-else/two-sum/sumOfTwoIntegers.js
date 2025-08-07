5f1290c9651197ef8c6d4736f7042ffb97516001
giribabu-a <giribabuannapareddi@gmail.com>
2025-08-07 12:45:47 +0530

    Two Sum

--- 2. Functions & if-else/two-sum/sumOfTwoIntegers.js
+++ 2. Functions & if-else/two-sum/sumOfTwoIntegers.js
@@ -1,22 +1,4 @@
-/* 
-    Sum of Two Integers
-    
-    Problem Statement::
-    Write a Program that defines a function to calculate the sum of two integers and prints the result.
-    Call this function by passing two integer values.
-
-    Approach::
-    1) Define a function that takes two numbers as input.
-    2) Add the two numbers inside the function.
-    3) Call the function with two integers & print the result.
-
-    Example::
-    1) Input: 5, 3
-    2) Process: a + b => 5 + 3 = 8
-    3) Output: 8
-*/
-
-function sum(a, b){
+function sum(a, b) {
     let add = a + b;
     console.log(add)
 }
