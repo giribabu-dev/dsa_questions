a168f9f9d57211cb7b3c331c22094f0628ed5dc6
giribabu-a <giribabuannapareddi@gmail.com>
2025-08-07 12:35:30 +0530

    Even or Odd Number

---
+++ 2. Functions & if-else/even-or-odd/checkEvenOrOdd.js
@@ -0,0 +1,11 @@
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        console.log("Even Number")
+    }
+    else {
+        console.log("Odd Number")
+    }
+}
+
+checkEvenOrOdd(18)
+checkEvenOrOdd(5)
