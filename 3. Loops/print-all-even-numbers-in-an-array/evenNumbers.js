cd585427fee1ae1baff2b654198a3b257753f534
giribabu-a <giribabuannapareddi@gmail.com>
2025-08-10 17:06:07 +0530

    Even Numbers

--- 3. Loops/print-all-even-numbers-in-an-array/evenNumbers.js
+++ 3. Loops/print-all-even-numbers-in-an-array/evenNumbers.js
@@ -1,15 +1,7 @@
-/* 
-    Loops 1
-    Print all even numbers in an array
-*/
+let arr = [10, 3, 5, 2, 7, 6, 7]
 
-function printEvenNumbers(arr) {
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 == 0) {
-            console.log(arr[i])
-        }
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i])
     }
-};
-
-printEvenNumbers([5, 10, 7, 1, 20, 25]);
+}
