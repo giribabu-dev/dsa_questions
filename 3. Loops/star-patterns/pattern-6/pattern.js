477f121d96cfc8c685130e88483fb444a51aaa2e
giribabu-a <giribabuannapareddi@gmail.com>
2025-08-11 11:57:39 +0530

    Pattern-7 in Star patterns

--- 3. Loops/star-patterns/pattern-6/pattern.js
+++ 3. Loops/star-patterns/pattern-6/pattern.js
@@ -1,12 +1,12 @@
 let n = 4;
 
-for(let i = 0; i < n; i++){
+for (let i = 0; i < n; i++) {
     let row = "";
-    for(let j = 0; j < (n-i-1); j++){
+    for (let j = 0; j < n - (i + 1); j++) {
         row = row + " ";
     }
 
-    for(let k = 0; k <= i; k++){
+    for (let k = 0; k < i + 1; k++) {
         row = row + "*";
     }
     console.log(row)
