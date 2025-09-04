534a07071807d491641573e5104f53b323532a12
giribabu-a <giribabuannapareddi@gmail.com>
2025-09-04 23:03:36 +0530

    changes

--- 4_Arrays/4_buy-sell-stocks/maxProfit2.js
+++ 4_Arrays/4_buy-sell-stocks/maxProfit2.js
@@ -1,12 +1,12 @@
-var maxProfit = function(prices){
+var maxProfit = function (prices) {
     let minPrice = Infinity;
     let maxProfit = 0;
 
-    for(let price of prices){
-        if(price < minPrice){
+    for (let price of prices) {
+        if (price < minPrice) {
             minPrice = price;
         }
-        else if((price - minPrice) > maxProfit){
+        else if ((price - minPrice) > maxProfit) {
             maxProfit = price - minPrice;
         }
     }
