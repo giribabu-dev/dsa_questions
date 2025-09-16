ed6bb094867a1fe875caf850a1f6a00c087e44f6
giribabu-a <giribabuannapareddi@gmail.com>
2025-09-16 19:34:16 +0530

    changes

--- 5_Recursion/4_sum-all-numbers/sumAllNumbers.js
+++ 5_Recursion/4_sum-all-numbers/sumAllNumbers.js
@@ -1,8 +1,8 @@
 let arr = [5, 2, 6, 1, 3];
 
 function sum(n) {
-    if (n == 0) return arr[0];
+    if (n === 0) return arr[0];
     return arr[n] + sum(n - 1);
 }
 
-console.log(sum(arr.length - 1));
+console.log(sum(arr.length - 1)); // 17
